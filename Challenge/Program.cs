a14f4ee28f8369b869ddae80ed3bc2b13690f430
YusuffAhmad <97904018+YusuffAhmad@users.noreply.github.com>
2022-03-04 12:24:56 +0100

    CLH Code Challenge Answer

---
+++ Challenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] MyArray = { "abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+            var result = GetMaximumLenght(MyArray);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+        public static int GetMaximumLenght(string[] arr)
+        {
+            int MaxLenght = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (!arr[i].Equals(arr[j]))
+                    {
+                        MaxLenght = arr[i].Length * arr[j].Length;
+                        break;
+                    }
+                    else
+                    {
+                        MaxLenght = 0;
+                    }
+                }
+            }
+            return MaxLenght;
+        }
+    }
+}
